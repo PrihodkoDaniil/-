8f9bdfe52c5d9a18d95e559fc9a9a64e4a9170fa
PrihodkoDaniil <led20012209@gmail.com>
2024-03-14 14:14:58 +0300

    update

--- Player.gd
+++ Player.gd
@@ -3,11 +3,19 @@ extends CharacterBody2D
 var speed = 200
 var direction = Vector2.ZERO
 @onready var animation = $AnimatedSprite2D
+@onready var actionable_finder: Area2D = $Direction/ActionableFinder
+
 func movement():
 	velocity = Vector2.ZERO
 	direction = Vector2(Input.get_axis("left","right"),Input.get_axis("up","down"))
 	velocity = direction.normalized()*speed
 	move_and_slide()
+	#dialogue_lol
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionables = actionable_finder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
 	#add animated
 	if velocity== Vector2.ZERO:
 		$AnimatedSprite2D.play("idle")
