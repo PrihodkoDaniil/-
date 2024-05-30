761a8f78919e4e4cc0b973d2f85bc53903860d1b
Krevetka0 <92375515+Krevetka0@users.noreply.github.com>
2024-05-30 19:00:25 +0300

    final

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	load_game()
 
 
 func _on_settings_pressed():
@@ -11,3 +11,16 @@ func _on_settings_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+func load_game():
+	if not FileAccess.file_exists("user://savegame.save"):
+		get_tree().change_scene_to_file("res://world.tscn")
+	else:
+		var save_game = FileAccess.open("user://savegame.save", FileAccess.READ)
+		while save_game.get_position() < save_game.get_length():
+			var json_string  = save_game.get_line()
+			var json = JSON.new()
+			var parse_result = json.parse(json_string)
+			var node_data = json.get_data()
+			var act = node_data["act"]
+			get_tree().change_scene_to_file(act)
